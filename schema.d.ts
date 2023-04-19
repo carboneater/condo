9c0679cc9f4c94fcfaee58b56e2d141b348ae80d
gfournier <gfournier@focusoptimization.com>
2023-04-19 15:25:59 -0400

    chore: eslint & prettier config

--- schema.d.ts
+++ schema.d.ts
@@ -1,12 +1,8 @@
 export type ActionType = 'inspection'|'maintenance'|'repair'
 
-export interface Action {
-	description?: string
-	entity?: unknown
-	name: string
-	type: ActionType
-	ttl?: number
+export interface Asset {name: string, id: number,
+	ttl?: Partial<{lifetime?: number|string} & Record<ActionType, number|string>>,
+	unit?: number
 }
 
-export interface Asset {name: string, id: number, unit?: number}
 export interface LogEntry {assetId: number, date: string, type: ActionType}
