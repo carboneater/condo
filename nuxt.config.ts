391ab77d97f6013036011c65d627fc120349d074
Gabriel Fournier <gab@gfournier.ca>
2023-12-30 04:15:28 +0000

    fix: Upgrade Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["vue-final-modal/style.css"],
-  modules: ["@nuxt/devtools", "@nuxtjs/tailwindcss", "nuxt-vitest"],
+  modules: ["@nuxt/devtools", "@nuxtjs/tailwindcss"],
   typescript: {
     shim: false,
     strict: true,
