c5df1c2e37b079efeec46e1def16feef3cf8db8e
Gabriel Fournier <gab@gfournier.ca>
2025-03-28 01:50:15 +0000

    refact: SQL Rebuilding, yay

--- server/routes/building.get.ts
+++ server/routes/building.get.ts
@@ -5,11 +5,9 @@ import sql from "../../sql";
 export default eventHandler(async () => {
   const result = await sql`SELECT * FROM buildings;`;
   return {
-    constructionDate: Temporal.PlainDate.from(
-      result[0].construction_date.toISOString().split("T")[0]
-    ),
+    constructionDate: Temporal.PlainDate.from(result[0].construction_date.toISOString().split("T")[0]),
     id: result[0].id,
     name: result[0].name,
-    unitsCount: result[0].unit_count,
+    unitsCount: result[0].units,
   } as Building;
 });
